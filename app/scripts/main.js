8a3d682f0902ba00665936293b96479f95051798
Thomas Duval <thomas.duval@opmbx.org>
2016-02-16 08:57:58 +0100

    Improve design & trigger on click

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,22 @@
 /* global $ */
-if (navigator.geolocation) {
-    var lat = 35;
-    var lon = 139;
-    navigator.geolocation.getCurrentPosition(function (position) {
-        lat = position.coords.latitude;
-        lon = position.coords.longitude;
-        var owpApi = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=metric&appid=d0256561cef041c12aa806862c96034c';
-        $("#description").html(owpApi);
-        $.getJSON(owpApi, function (json) {
-            $("#temperature").html(json.main.temp);
-            $("#city").html(json.name);
-            $("#description").html(json.weather[0].description);
-            $("#wind").html(json.wind.speed);
-            var icon = "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png";
-            $("#icon").attr("src", icon)
+$("#getWeather").click(function () {
+    if (navigator.geolocation) {
+        var lat;
+        var lon;
+        navigator.geolocation.getCurrentPosition(function (position) {
+            lat = position.coords.latitude;
+            lon = position.coords.longitude;
+            var owpApi = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=metric&appid=d0256561cef041c12aa806862c96034c';
+            $("#description").html(owpApi);
+            $.getJSON(owpApi, function (json) {
+                $("#temperature").html(json.main.temp);
+                $("#city").html(json.name);
+                $("#description").html(json.weather[0].description);
+                $("#wind").html(json.wind.speed);
+                var icon = "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png";
+                $("#icon").attr("src", icon)
+            });
         });
-    });
 
-}
+    }
+});
