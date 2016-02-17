447e2c37bbc7837c6c7e7cf69a6e8f50ff9cbb1a
Thomas Duval <thomas.duval@opmbx.org>
2016-02-17 10:24:27 +0100

    Add buttons to get weather & change units

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,12 @@
 /* global $ */
-$("#getWeather").click(function () {
+function owpFunction(unit) {
     if (navigator.geolocation) {
-        var lat;
-        var lon;
         navigator.geolocation.getCurrentPosition(function (position) {
-            lat = position.coords.latitude;
-            lon = position.coords.longitude;
-            var owpApi = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=metric&appid=d0256561cef041c12aa806862c96034c';
-            $("#description").html(owpApi);
-            $.getJSON(owpApi, function (json) {
+            var lat = position.coords.latitude;
+            var lon = position.coords.longitude;
+            var owpApiUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=' + unit + '&appid=d0256561cef041c12aa806862c96034c';
+            // $("#description").html(owpApiUrl);
+            $.getJSON(owpApiUrl, function (json) {
                 $("#temperature").html(json.main.temp);
                 $("#city").html(json.name);
                 $("#description").html(json.weather[0].description);
@@ -19,4 +17,22 @@ $("#getWeather").click(function () {
         });
 
     }
+};
+
+$("#getWeather").click(function () {
+    var unit = 'metric';
+    owpFunction(unit);
+});
+
+$("#fUnit").click(function () {
+    var unit = 'imperial';
+    owpFunction(unit);
+    $("#temperatureUnit").html(" °F")
+    $("#windUnit").html(" miles/hour")
+});
+$("#cUnit").click(function () {
+    var unit = 'metric';
+    owpFunction(unit);
+    $("#temperatureUnit").html(" °C")
+    $("#windUnit").html(" meter/sec")
 });
